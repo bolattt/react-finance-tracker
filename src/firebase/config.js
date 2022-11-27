8e22070a9fb6b7ab7c9fd77437c3a1600297a3cf
Bo Latt <bolattny@gmail.com>
2022-11-27 11:20:16 -0500

    add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAO_G7iKQbZrr8PHJuPC4ZqvwKljqofSZ8",
+  authDomain: "mymyone-730f1.firebaseapp.com",
+  projectId: "mymyone-730f1",
+  storageBucket: "mymyone-730f1.appspot.com",
+  messagingSenderId: "910552343955",
+  appId: "1:910552343955:web:52255a0765b190d4003466",
+  measurementId: "G-SPNLKK9QLZ",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
