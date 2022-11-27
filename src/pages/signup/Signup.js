0310f6d43dadff24645f286953f618ebddf65ae2
Bo Latt <bolattny@gmail.com>
2022-11-27 11:04:24 -0500

    add signup form and css

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,6 +1,45 @@
+import { useState } from "react";
 import styles from "./Signup.module.css";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password);
+  };
+
+  return (
+    <form className={styles["signup-form"]} onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </label>
+      <label>
+        Email
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Sumbit</button>
+    </form>
+  );
 };
 
 export default Signup;
